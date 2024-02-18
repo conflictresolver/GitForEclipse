f1390a8e7661500cbee8c6141a43018a2e169fed
mathpro <mathpro740@gmail.com>
2024-02-18 14:26:16 +0900

    add many to gitigonre

--- src/com/learner/Gitconflict.java
+++ src/com/learner/Gitconflict.java
@@ -4,7 +4,7 @@ public class Gitconflict {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I will like you");
+		System.out.println("I will fuck you");
 
 	}
 
