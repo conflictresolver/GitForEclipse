d07d6beea2f5ae9069010c6818bdaceba4633b4d
mathpro <mathpro740@gmail.com>
2024-02-18 16:14:32 +0900

    resolve

--- src/com/learner/Gitconflict.java
+++ src/com/learner/Gitconflict.java
@@ -4,11 +4,7 @@ public class Gitconflict {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("I will fuck you");
-=======
 		System.out.println("one word is enough to know everythingu");
->>>>>>> feature2
 
 	}
 
