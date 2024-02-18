d4571032b655c01908f975e6bf1d163dd563dc60
mathpro <mathpro740@gmail.com>
2024-02-18 19:20:00 +0900

    end

--- src/com/learner/Gitconflict.java
+++ src/com/learner/Gitconflict.java
@@ -4,7 +4,7 @@ public class Gitconflict {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("cheet sheet");
+		System.out.println("this is the end");
 
 	}
 
